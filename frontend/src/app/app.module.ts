1fbaac7f805b8d3f08f6f2058779284d03a97e21
Pooja Dogra <poojadogra2930@gmail.com>
2023-03-19 00:27:32 +0530

    first commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http'; 
 import { AdduserComponent } from './components/adduser/adduser.component';
 import { UpdateuserComponent } from './components/updateuser/updateuser.component';
 import { ViewusersComponent } from './components/viewusers/viewusers.component';
